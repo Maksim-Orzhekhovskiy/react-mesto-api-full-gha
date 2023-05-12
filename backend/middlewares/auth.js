ed9d1c059baee38f7c161d246614e3a855b2b554
Maksim Orzhekhovskiy <smiley.dude@yandex.ru>
2023-05-12 18:58:33 +0600

    dev26

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,23 +1,27 @@
-const UnauthorizedError = require("../errors/unauthorizedError");
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 const { NODE_ENV, JWT_SECRET } = process.env;
+const UnauthorizedError = require("../errors/unauthorizedError");
 
+function extractBearerToken(header) {
+  return header.replace('Bearer ', '');
+}
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
-  if (!token) {
-    return next(new UnauthorizedError("Необходима авторизация3"));
-  }
-  let payload;
-  try {
-    payload = jwt.verify(
-      token,
-      NODE_ENV === "production" ? JWT_SECRET : "dev-secret-key",
-    );
-  } catch (err) {
-    return next(new UnauthorizedError("Необходима авторизация4"));
-  }
-  req.user = payload;
-  return next();
-};
+  const { authorization } = req.headers;
 
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError("Необходима авторизация3");
+  }
+  const token = extractBearerToken(authorization);
+  let payload;
+
+  try {
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret-key');
+  } catch (err) {
+    throw new UnauthorizedError("Необходима авторизация4");
+  }
+
+  req.user = payload;
+
+  next();
+};
